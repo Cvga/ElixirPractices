51cb0bc80baaf40ac8b716ed8079cfc6d9a5dcc1
Cvga <carvega07@gmail.com>
2023-05-15 20:09:43 -0600

    New cards functions added

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -13,7 +13,24 @@ defmodule Cards do
 
   """
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Hearts", "Clubs", "Diamonds" ]
+
+    for suit <- suits, value <-values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def cards_shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 
 end
